9e8ed76f1bdf6bc63e7e5afc7e07e7d7ca90c142
wep1980 <wepbike@gmail.com>
2021-10-24 14:31:47 -0300

    Seeding inicial para BD, CommandLineRunner metodo run() com BCryptPasswordEncoder

--- src/main/java/br/com/wepdev/usuario/UsuarioApplication.java
+++ src/main/java/br/com/wepdev/usuario/UsuarioApplication.java
@@ -1,15 +1,30 @@
 package br.com.wepdev.usuario;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableEurekaClient // Habilitando a configuração do servidor eureka
 @SpringBootApplication
-public class UsuarioApplication {
+public class UsuarioApplication implements CommandLineRunner { // CommandLineRunner imprime no console o bcrypt de uma senha passada no metodo run()
+
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder; // precisou ser criado um bean, para ter a instancia do bcrypy disponivel no spring
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(UsuarioApplication.class, args);
     }
 
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        //System.out.println("BCRYPT = " + passwordEncoder.encode("123456"));
+    }
 }
