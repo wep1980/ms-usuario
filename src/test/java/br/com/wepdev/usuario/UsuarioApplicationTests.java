252875e64b7f0220b6b361e011a1ae7532c6ac2b
wep1980 <wepbike@gmail.com>
2021-10-24 13:18:16 -0300

    Primeiro commit, projeto criado

---
+++ src/test/java/br/com/wepdev/usuario/UsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wepdev.usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
