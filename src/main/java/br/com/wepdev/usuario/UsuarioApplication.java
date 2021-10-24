f8bac6ba917d239fe52d5184e8a87d79e119a921
wep1980 <wepbike@gmail.com>
2021-10-24 13:24:11 -0300

    Configurando o application.properties

--- src/main/java/br/com/wepdev/usuario/UsuarioApplication.java
+++ src/main/java/br/com/wepdev/usuario/UsuarioApplication.java
@@ -2,7 +2,9 @@ package br.com.wepdev.usuario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient // Habilitando a configuração do servidor eureka
 @SpringBootApplication
 public class UsuarioApplication {
 
